e6e1d4f235c21a0a93b475fa2e22fa09b3296abe
n13PAG <74218503+n13PAG@users.noreply.github.com>
2025-05-07 02:44:38 -0400

    Update race strategy page and implement js file for page animation

---
+++ static/race_strategy.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible');
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  document.querySelectorAll('.slide-in-left, .slide-in-right').forEach(el => {
+    observer.observe(el);
+  });
+});
